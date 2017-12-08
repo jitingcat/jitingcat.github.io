b54866ef302fa23b0721b11f49039972b4704d64
寂听 <jitingchina@gmail.com>
2017-12-08 17:55:33 +0800

    修正代码及时间戳

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ev
+
+git clone https://github.com/${GitHubUser}/${GitHubRepo}.git hexo/.blog_deploy #git clone site file to hexo/.blog_deploy
+cd hexo/.blog_deploy 
+git checkout master
+
+cd ../  #in hexo folder
+
+mv .blog_deploy/.git/ ./public/
+
+cd ./public
+
+#  - git init
+#  - git config --global push.default matching
+git config --global user.name "${GitHubUser}"
+git config --global user.email "${GitHubEmail}"
+
+git add .
+git commit -m "site updated: `date +"%Y-%m-%d %H:%M:%S"`"
+
+git push --quiet --force https://${GitHubToken}@github.com/${GitHubUser}/${GitHubRepo}.git master:master 
